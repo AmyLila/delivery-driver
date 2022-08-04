86bd55d38664cbfd8dae5ea5dcb97c50938d109e
shakerbaker78 <bakeamym@yahoo.com>
2022-08-03 21:28:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const connection = require('./data/connection');
+const port = process.env.PORT || 3000;
+const app = express();
+require('dotenv').config();
+
+
+//Connection to mongoDB
+// connection.mongoConnection();
+
+app
+.use(express.json())
+.use('/', require('./routes'))
+
+
+
+app.listen(port, () => {
+  console.log(`Running on port ${port} from the index file`)
+
+});
